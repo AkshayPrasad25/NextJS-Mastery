d6c9febb57c9a614c488843d224d1ca436f82b83
AkshayPrasad25 <akshayprasad510@gmail.com>
2023-10-08 20:03:31 +0530

    Blog Changes

--- huntingcoder/pages/About.js
+++ huntingcoder/pages/About.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import styles from '../styles/Home.module.css'
 
 const About = () => {
   return (
-    <div>This is my about component</div>
+    <div className={styles.aboutwrap}>
+      <div className={styles.about}>
+        <h2>Introduction:</h2>
+        <p>Hello and welcome to Hunting coders! I'm Castiel, the creator and writer behind this blog. 
+          I'm thrilled that you've stopped by to learn more about me and the purpose of this space</p>
+        <h2>Who am I?</h2>
+        <p>I'm a versatile and innovative developer with skill set in front-end development, Google Cloud Platform, and Python, 
+          makes me capable of turning ideas into interactive realities that leave a lasting impact.</p>
+        <h2>What You'll find here:</h2>
+        <p>You will find my best blogs which are related to web development and cloud technologies.</p>
+        <h2>Connect with me:</h2>
+        <p>I love interacting with my readers, so please feel free to connect with me on social media platforms like instagram. 
+          You can also reach out to me via email at akshaywork510@gmail.com for any inquiries or collaborations.</p>
+        <h2>
+          Thank you for visiting:
+        </h2>
+        <p>
+        Thank you for taking the time to get to know me and explore my blog. I'm excited to embark on this journey with you, and I can't wait to share more valuable content in the future.
+        </p>
+      </div>
+    </div>
+    
   )
 }
 
